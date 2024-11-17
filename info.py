8d2c452952ea26c42e276c0ed962f2c7835de6fa
Zingke99 <135798795+Zingke99@users.noreply.github.com>
2024-11-17 10:30:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -62,7 +62,7 @@ if MULTIPLE_DATABASE == False:
 else:
     USER_DB_URI = DATABASE_URI # This Db is for User Data Store
     OTHER_DB_URI = environ.get('OTHER_DB_URI', "mongodb+srv://lokibhai095:lokibhai095@cluster0.69dls.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # This Db Is For Other Data Store
-    FILE_DB_URI = environ.get('FILE_DB_URI', "mongodb+srv://Koi:Lok@cluster0.bt8r7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # This Db Is For File Data Store
+    FILE_DB_URI = environ.get('FILE_DB_URI', "mongodb+srv://ramparshadbismillah:nirudas123@cluster0.xkwhj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # This Db Is For File Data Store
     SEC_FILE_DB_URI = environ.get('SEC_FILE_DB_URI', "mongodb+srv://swamibaba079:swamidas079@cluster0.tfebd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # This Db is for File Data Store When First Db Is Going To Be Full.
     
 DATABASE_NAME = environ.get('DATABASE_NAME', "techvjfilterbot")
